6d69f794f9bdade1e0204cd1a5b1ebd1fb8ff6e7
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2023-10-16 12:03:56 +0200

    Repo fixes

--- utilities/build.sh
+++ utilities/build.sh
@@ -2,5 +2,7 @@
 #
 
 echo "Starting build process..."
-podman run --platform linux/amd64 -v "./:/antora" antora/antora site.yml
+podman run --rm --platform linux/amd64 -v "./:/antora:Z" docker.io/antora/antora site.yml
 echo "Build process complete. Check the ./www folder for the generated site."
+
+podman run --rm --platform linux/amd64 -v /opt/showroom/content:/antora:Z docker.io/antora/antora site.yml
