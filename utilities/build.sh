5d9331bf4cefd7b90f378cb4631889b322064e0d
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2023-12-20 08:53:06 +0100

    Update build scripts

--- utilities/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#
-
-echo "Starting build process..."
-podman run --rm --platform linux/amd64 -v "./:/antora:Z" docker.io/antora/antora site.yml
-echo "Build process complete. Check the ./www folder for the generated site."
-
-podman run --rm --platform linux/amd64 -v /opt/showroom/content:/antora:Z docker.io/antora/antora site.yml
